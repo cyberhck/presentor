7941576f01d38b7e9fc049d20b46b66d4bd8210a
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-03-05 22:21:20 +0530

    one automatic installer for lazy ones like me :P

---
+++ installer.sh
@@ -0,0 +1,17 @@
+echo "Cloning Presentor!!"
+git clone https://github.com/cyberhck/presentor.git --depth 1
+echo "Entering into presentor"
+sleep 1
+cd presentor
+pwd
+sleep 1
+echo "Execcuting npm install"
+npm install
+echo "removing .tempFile from reveal"
+rm presentation/reveal.js/.tempFile
+echo "cloning reveal.js"
+sleep 1
+git clone https://github.com/hakimel/reveal.js.git presentation/reveal.js --depth 1
+echo "copying presentor.js inside respective directory"
+cp presentor.js presentation/reveal.js/presentor.js
+echo "Done!"
