ed2e4664376ce58851908462fdf8e42f88ba9d40
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-03-08 08:15:54 +0530

    console.log in browser

--- presentor.js
+++ presentor.js
@@ -1,8 +1,10 @@
 var socket=io();
 socket.on('chat',function(data){
 	if(data=="next"){
+		console.log("Next");
 		Reveal.next()
 	}else if(data=="prev"){
+		console.log("Prev");
 		Reveal.prev();
 	}
 });
