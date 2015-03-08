5b889f0255441469641e53fce766469c18076e05
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-03-08 11:54:59 +0530

    installer split to reveal.js and impress.js

---
+++ installer-impress.js
@@ -0,0 +1,17 @@
+echo "Cloning Presentor!!"
+git clone https://github.com/cyberhck/presentor.git --depth 1
+echo "Entering into presentor"
+sleep 1
+cd presentor
+pwd
+sleep 1
+echo "Executing npm install"
+npm install
+echo "removing .tempFile from presentation"
+rm presentation/.tempFile
+echo "cloning reveal.js"
+sleep 1
+git clone https://github.com/bartaz/impress.js.git presentation --depth 1
+echo "copying presentor.js inside respective directory"
+cp presentor.js presentation/presentor.js
+echo "Done!"
